d2f778cdede546854ea542f522794cf9f5ca51e8
NickCoppa <36050969+NickCoppa@users.noreply.github.com>
2018-10-18 18:32:15 -0400

    Update set_mpi_path.sh

--- set_mpi_path.sh
+++ set_mpi_path.sh
@@ -1,2 +1,3 @@
 echo "export PATH='$PATH:/software/openmpi/3.1.2/bin'" >> /users/nc835571/.bashrc
-echo "export LD_LIBRARY_PATH='$LD_LIBRARY_PATH:/software/openmpi/3.1.2/lib/'" >> /users/nc835571/.bashrcset
+echo "export LD_LIBRARY_PATH='$LD_LIBRARY_PATH:/software/openmpi/3.1.2/lib/'" >> /users/nc835571/.bashrc
+source /users/nc835571/.bashrc
